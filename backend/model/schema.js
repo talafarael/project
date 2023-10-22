b275d4a18c8a6d8b1f8e3cca10ee6bfbc89f15cc
talafarael <127351345+talafarael@users.noreply.github.com>
2023-10-22 13:05:41 +0300

    Create schema.js

---
+++ backend/model/schema.js
@@ -0,0 +1,11 @@
+const {model,Schema}=require('mongoose')
+
+const User=new Schema({
+    username:{type: String,uniqne:true,required: true},
+    email:{type: String,uniqne:true,required: true},
+    password:{type: String,required: true},
+    role:{type: String,uniqne:true,required: true}
+    }
+    
+)
+module.exports=model('User',User)
