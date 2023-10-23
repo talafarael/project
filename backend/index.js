c154ab9c186b6563dcc457b71f137dc666af3749
talafarael <127351345+talafarael@users.noreply.github.com>
2023-10-23 13:34:59 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
 const express=require('express')
 const mongoose=require('mongoose')
 const app=express()
+const path = require('path');
 const PORT=process.env.PORT||3000
 const authRouter =require('./authRouterLog')
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname,'ejs'));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'ejs')));
 app.use(express.json())
 app.use('/auth',authRouter)
 start=async()=>{ try{
+
      await mongoose.connect('mongodb+srv://farael:farae1aer@cluster0.pkknndw.mongodb.net/')
-    app.listen(PORT,()=>{console.log('server run')})
+     app.get('/', (req, res) => {
+        res.render('login');
+    });  
+      app.listen(PORT,()=>{console.log('server run')})
 }catch(e){
         console.log(e)
     }
