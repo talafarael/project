69180078265f91c840ce6d4f48b3eeda93beb07b
talafarael <127351345+talafarael@users.noreply.github.com>
2023-10-24 11:02:46 +0300

    Update authRouterLog.js

--- backend/authRouterLog.js
+++ backend/authRouterLog.js
@@ -4,5 +4,6 @@ const login=require('./authlogin')
 
 router.post('/register',login.register)
 router.get ('/sendemail',login.sendemail)
-
+router.post('/registercheck',login.registercheck)
+router.post('/login',login.login)
 module.exports=router
