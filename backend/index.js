5ca94f31beef318e8a53f7c781c47195327e9ae4
talafarael <127351345+talafarael@users.noreply.github.com>
2023-11-03 22:25:40 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,77 +1,87 @@
-const express=require('express')
-const mongoose=require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const authRouter =require('./authRouterLog')
-const app=express()
-const fs=require('fs')
-const Music=require('./model/music')
+const authRouter = require('./authRouterLog');
+const app = express();
+const fs = require('fs');
+const Music = require('./model/music');
 const bodyParser = require('body-parser');
 // var fileupload = require("express-fileupload");
-const cors = require("cors")
+const cors = require('cors');
 const path = require('path');
-const multer=require('multer');
+const multer = require('multer');
 const { required } = require('joi');
-const GridFsStorage=require('multer-gridfs-storage')
-const methodOverride=require('method-override')
-const Grid=require('gridfs-stream')
-const storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,'Images')
-    },filename:(req,file,cb)=>{
-        console.log(file)
-        cb(null,Date.now()+path.extname(file.originalname))
-    }
-})
-const upload=multer({
-    storage:storage
-})
-const PORT=process.env.PORT||3000
+const GridFsStorage = require('multer-gridfs-storage');
+const methodOverride = require('method-override');
+const Grid = require('gridfs-stream');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'Images');
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+const upload = multer({
+    storage: storage,
+});
+const PORT = process.env.PORT || 3000;
 // app.use(fileupload());
 app.set('view engine', 'ejs');
-app.set('views', path.resolve(__dirname,'ejs'));
+app.set('views', path.resolve(__dirname, 'ejs'));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("__method"))
+app.use(methodOverride('__method'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(express.static(path.join(__dirname,'ejs')));
+app.use(express.static(path.join(__dirname, 'ejs')));
 app.use(cors());
 
-app.use(express.json())
-app.post('/auth/musiccreate',upload.single('music1'),(req, res)=>{ 
+app.use(express.json());
+app.post('/auth/musiccreate', upload.single('music1'), (req, res) => {
     try {
         if (!req.file) {
             return res.status(400).json({ error: 'No file uploaded.' });
         }
+        const conn = mongoose.createConnection(process.env.MONGO);
+        let gfs;
+        conn.once('open', () => {
+            gfs = Grid(conn.db, mongoose.mongo);
+            gfs.collection('uploads');
+        });
 
         const music = new Music({
-            
             musicData: {
-                data:fs.readFileSync(path.join(__dirname,"images/",req.file.filename)) ,
-                contentType:'audio/mpeg'}
-
+                data: fs.readFileSync(
+                    path.join(__dirname, 'images/', req.file.filename)
+                ),
+                contentType: 'audio/mpeg',
+            },
         });
-      
+
         music.save();
-        return res.status(200).json({ message: 'Music file uploaded successfully.' });
+        return res
+            .status(200)
+            .json({ message: 'Music file uploaded successfully.' });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ error: 'Internal server error' });
     }
-})
-app.use('/auth',authRouter)
+});
+app.use('/auth', authRouter);
 
-try{
-
-     
-     app.get('/', (req, res) => {
+try {
+    app.get('/', (req, res) => {
         res.render('login');
-    });  
+    });
     app.get('/music', (req, res) => {
         res.render('musicload');
-    });  
-      app.listen(PORT,()=>{console.log('server run')})
-}catch(e){
-        console.log(e)
-    }
+    });
+    app.listen(PORT, () => {
+        console.log('server run');
+    });
+} catch (e) {
+    console.log(e);
+}
