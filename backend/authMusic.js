e38085ac244a72016ce593dcfbedc457d3c27a64
talafarael <127351345+talafarael@users.noreply.github.com>
2023-11-07 23:35:45 +0200

    change mongo path

--- backend/authMusic.js
+++ backend/authMusic.js
@@ -2,32 +2,38 @@ const util = require('util');
 const fs = require('fs');
 const writeFileAsync = util.promisify(fs.writeFile);
 const Music=require('./model/music')
-
+const path = require('path');
+const Songs=require('./model/song')
 class authMusic{
 
-   async musicCreate(req, res) {
-    const musicFile = req.files.music1; // Обратите внимание, что это поле 'music1' соответствует ключу, который вы указали в FormData
-    const contentType = musicFile.mimetype;
-console.log(contentType)
-    try {
-        const buffer = musicFile.data; // Получение буфера данных файла
-
-        
-        const music = new Music({
-            musicData: buffer,
-            contentType: contentType,
-            nema:'fafa'
-        });
-
-        await music.save();
-
-        console.log('Музыка успешно сохранена в базе данных.');
-        res.status(200).send('Музыка успешно сохранена в базе данных.');
-    } catch (error) {
-        console.error('Ошибка при сохранении музыки:', error);
-        res.status(500).send('Произошла ошибка при сохранении музыки.');
+    async musiccreate(req, res){ 
+        try {
+           const {name}=req.body
+            if (!req.file) {
+                return res.status(400).json({ error: 'No file uploaded.' });
+            }
+      console.log(name)
+            const music = new Music({
+                
+                
+                    data:fs.readFileSync(path.join(__dirname,"images/",req.file.filename)) ,
+                    contentType:'audio/mpeg'}
+    
+           );
+           const songs=new Songs({
+            autor:'',
+            songs:name,
+            idpath:music._id
+           })
+           songs.save()
+         
+            music.save();
+            return res.status(200).json({ message: 'Music file uploaded successfully.' });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
     }
-     }
 
    async getmusic(req,res){
     try{
--- backend/authRouterLog.js
+++ backend/authRouterLog.js
@@ -2,8 +2,21 @@ const Router=require('express')
 const router =new Router()
 const login=require('./authlogin')
 const authMusicController=require('./authMusic')
-
-// router.post('/musiccreate', authMusicController.musicCreate);
+const path = require('path');
+const multer=require('multer');
+const cors = require("cors")
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'Images')
+    },filename:(req,file,cb)=>{
+        console.log(file)
+        cb(null,Date.now()+path.extname(file.originalname))
+    }
+})
+const upload=multer({
+    storage:storage
+})
+router.post('/musiccreate',upload.single('music1'),authMusicController.musiccreate)
 router.get ('/getmusic',authMusicController.getmusic)
 router.post('/register',login.register)
 router.get ('/sendemail',login.sendemail)
