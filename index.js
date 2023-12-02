8352a8720f09e13da93190cf0eaed2ac508bc762
talafarael <127351345+talafarael@users.noreply.github.com>
2023-12-02 12:12:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
   origin:process.env.ACCESSCOOKIE, // Разрешить запросы с любых источников (можно указать конкретный домен)
-  
+  secure: true
   credentials: true,// Разрешение передачи куки и авторизационных заголовков
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type, Authorization'
