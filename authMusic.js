a9924a36d0f0cdab88ba49914e499376ec83db45
talafarael <127351345+talafarael@users.noreply.github.com>
2023-12-04 09:04:39 +0200

    Update authMusic.js

--- authMusic.js
+++ authMusic.js
@@ -1,27 +1,29 @@
 const util = require('util');
 const fs = require('fs');
-const User=require('./model/schema')
+const User = require('./model/schema');
 const jwt = require('jsonwebtoken');
 const writeFileAsync = util.promisify(fs.writeFile);
 const Music = require('./model/music');
 const path = require('path');
 const Songs = require('./model/song');
-const Autors=require('./model/autor')
+const Autors = require('./model/autor');
 class authMusic {
-    async  musiclike(req, res) {
+    async musiclike(req, res) {
         try {
             const { like_id } = req.body;
             const token = req.cookies.token;
             const decodedData = await jwt.verify(token, process.env.secret);
             const id = decodedData.id;
-    
+
             const user = await User.findById(id);
             const song = await Songs.findById(like_id);
-    
+
             if (!user || !song) {
-                return res.status(404).json({ error: 'Користувач або пісню не знайдено' });
+                return res
+                    .status(404)
+                    .json({ error: 'Користувач або пісню не знайдено' });
             }
-    
+
             if (user.liker_songs.includes(like_id)) {
                 user.liker_songs.pull(like_id);
                 song.like -= 1;
@@ -29,17 +31,19 @@ class authMusic {
                 user.liker_songs.push(like_id);
                 song.like += 1;
             }
-    
+
             await user.save();
             await song.save();
-    
-            return res.status(200).json({ message: 'Музичний файл успішно оновлено.' });
+
+            return res
+                .status(200)
+                .json({ message: 'Музичний файл успішно оновлено.' });
         } catch (error) {
             console.error(error);
             return res.status(500).json({ error: 'Внутрішня помилка сервера' });
         }
     }
-    
+
     async musiccreate(req, res) {
         try {
             const { name } = req.body;
@@ -58,7 +62,7 @@ class authMusic {
                 autor: '',
                 songs: name,
                 idpath: music._id,
-                like:0
+                like: 0,
             });
             songs.save();
 
@@ -72,63 +76,59 @@ class authMusic {
             return res.status(500).json({ error: 'Internal server error' });
         }
     }
-    async getSongs(req,res){
-        try{
-            const {autor}=req.body
-            
-            const autors=await Autors.find({  autor:autor})
-           const music = await Songs.find({autor:autor});
-           const arr={
-            'autors':autors,
-            'music':[music]
-           }
-           console.log(arr)
+    async getSongs(req, res) {
+        try {
+            const { autor } = req.body;
+
+            const autors = await Autors.find({ autor: autor });
+            const music = await Songs.find({ autor: autor });
+            const arr = {
+                autors: autors,
+                music: [music],
+            };
+            console.log(arr);
             res.json(arr);
-        }catch(e){
+        } catch (e) {
             console.error('Ошибка при сохранении музыки:', e);
             res.status(500).send('Произошла ошибка при сохранении музыки.');
         }
-        }
-        async get_Songs_For_Creat_Mainpage(req,res){
-            try{
-                const music = await Songs.find()
-                const autor= await Autors.find()
-                const arr={
-                    music:music,
-                    autor:autor
-                }
-                console.log(arr)
-                return res
-                .status(200)
-                .json({arr});
-
-
-            }catch(error) {
-                console.error('Ошибка при сохранении музыки:', error);
-                res.status(500).send('Произошла ошибка при сохранении музыки.');
-            }
-            
-
-        }
-    async getmusic(req, res) {
+    }
+    async get_Songs_For_Creat_Mainpage(req, res) {
         try {
-            const {id}=req.body
-            console.log(id)
-            const songs = await Songs.findOne({ _id:id});
-            
-            const music = await Music.findOne({ _id:songs.idpath});
-            console.log(songs)
-            res.json({music,songs});
+            const music = await Songs.find().limit(5);
+console.log('afa')
+            const autor = await Autors.find().limit(5);
+
+            const arr = {
+                music: music,
+                autor: autor,
+            };
+            console.log(arr);
+            return res.status(200).json({ arr });
         } catch (error) {
             console.error('Ошибка при сохранении музыки:', error);
             res.status(500).send('Произошла ошибка при сохранении музыки.');
         }
     }
-    async test(req,res){
-        try{
-            const {mus}=req.body
-            console.log(mus)
-        }catch(error) {
+    async getmusic(req, res) {
+        try {
+            const { id } = req.body;
+            console.log(id);
+            const songs = await Songs.findOne({ _id: id });
+
+            const music = await Music.findOne({ _id: songs.idpath });
+            console.log(songs);
+            res.json({ music, songs });
+        } catch (error) {
+            console.error('Ошибка при сохранении музыки:', error);
+            res.status(500).send('Произошла ошибка при сохранении музыки.');
+        }
+    }
+    async test(req, res) {
+        try {
+            const { mus } = req.body;
+            console.log(mus);
+        } catch (error) {
             console.error('Ошибка при сохранении музыки:', error);
             res.status(500).send('Произошла ошибка при сохранении музыки.');
         }
